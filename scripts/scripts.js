739b5930d640eb1ca948016dac723f527cf7ddf9
karvaroz <vargasriosk@gmail.com>
2022-04-05 14:48:12 -0500

    script de subir productos terminado

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,14 +24,13 @@ const getData = async (url) => {
   listarCard.innerHTML = "";
 
   data.forEach((product) => {
-    const { name, price, description, type, category, image } = product;
+    const { name, price, type, category, image } = product;
     listarCard.innerHTML += `
         <div class="card mt-5" style="width: 18rem;">
             <img class="card-img-top" src="${image}" alt="Card image cap" width="100" height="auto">
             <div class="card-body">
                 <h4 class="card-text">Nombre: ${name}</h4>
                 <p class="card-text">Precio: $${price}</p>
-                <p class="card-text">${description}</p>
                 <p class="card-text">Tipo: ${type}</p>
                 <p class="card-text">Categoría: ${category}</p>
             </div>
